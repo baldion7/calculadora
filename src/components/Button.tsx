992f4ae77b4d0357a594f76b1e2b2d129ebe847f
baldion7 <73363421+baldion7@users.noreply.github.com>
2022-09-25 14:45:10 -0500

    CALCULADORA RECAT NATIVE

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ThemeContext } from "../context/ThemeContext";
+import { Styles } from "../styles/GlobalStyles";
+interface ButtonProps {
+    onPress: () => void;
+    title: string;
+    isBlue?: boolean;
+    isGray?: boolean;
+}
+export default function Button({ title, onPress, isBlue, isGray }: ButtonProps) {
+    const theme = useContext(ThemeContext);
+
+    return (
+        < TouchableOpacity
+            style={
+                isBlue
+                    ? Styles.btnBlue
+                    : isGray
+                        ? Styles.btnGray
+                        : theme === "light"
+                            ? Styles.btnLight
+                            : Styles.btnDark
+            }
+            onPress={onPress}>
+            <Text
+                style={
+                    isBlue || isGray
+                        ? Styles.smallTextDark
+                        : theme === "dark"
+                            ? Styles.smallTexttight
+                            : Styles.smallTextDark
+                }
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+}
